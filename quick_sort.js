9e7bef83ec6751aecd809657a4b498ed90b7f8ca
Dmitry Evtifeev <d.evtifeev@dunice.net>
2018-07-20 14:30:54 +0300

    Fixed quick sort

--- quick_sort.js
+++ quick_sort.js
@@ -1,30 +1,64 @@
-const notSortedArray = require('./create_array')(10);
+const notSortedArray = require('./create_array')(10000);
 const notSortedArray_2 = [...notSortedArray];
 
 // Start sorting array process
 console.time('quick sort process with loop');
 
-function quickSort(inputArray) {
-    const number = inputArray.length;
-    const array_1 = [];
-    const array_2 = [];
-    const instance = inputArray[0];
-    for (let i = 1; i < number; i++) {
-        if (inputArray[i] < instance) {
-            array_1.push(inputArray[i]);
-        } 
-        else {
-            array_2.push(inputArray[i]);
-        }
-     }
-    return quickSort(array_1).concat(instance, quickSort(array_2));
+function swap(items, firstIndex, secondIndex){
+    const temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
 }
 
-quickSort(notSortedArray);
+function partition(items, left, right) {
+    let pivot = items[Math.floor((right + left) / 2)];
+    let i = left;
+    let j = right;
 
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
+}
+
+function quickSort(items, left, right) {
+
+    let index;
+
+    if (items.length > 1) {
+
+        index = partition(items, left, right);
+
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+
+    }
+
+    return items;
+}
+
+const arr = quickSort(notSortedArray, 0, notSortedArray.length - 1)
+console.log(arr.slice(arr.length - 10, arr.length))
 console.timeEnd('quick sort process with loop');
 
 // Start sorting array process by native sort method
 console.time('sort process with sort method');
-notSortedArray_2.sort();
+let sortarr = notSortedArray_2.sort();
+console.log(arr.slice(sortarr.length - 10, sortarr.length))
 console.timeEnd('sort process with sort method');
